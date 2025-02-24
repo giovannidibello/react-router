67174aa3c5d73cd56a0030c90d7ec7f062c1f403
Giovanni <dibellogio@gmail.com>
2025-02-24 16:04:08 +0100

    add posts list to api call

--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -1,12 +1,58 @@
 // importiamo componenti di template
 import Header from "./../components/Header";
 
+import { useState, useEffect } from 'react'
+import axios from "axios";
+
 export default function PostsList() {
 
+    const [postsList, setPostsList] = useState([]);
+
+    // funzione chiamata api tramite axios  
+
+    function fetchTodos() {
+        axios.get("http://localhost:3000/posts/")
+            .then((res) => setPostsList(res.data))
+    }
+
+    // chiamata funzione solo al primo rendering
+    useEffect(fetchTodos, []);
+
+    // funzione rimozione post
+    const removePost = (id) => {
+        const updatedList = postsList.filter((post) => {
+            return post.id !== id
+
+        });
+        setPostsList(updatedList);
+
+        if (updatedList.length === 0) {
+            return <h1>Non ci sono posts</h1>
+        }
+
+    }
+
     return (
+
         <>
             <Header />
-            <h2>PostsList</h2>
+            {postsList.length === 0 ? (
+
+                <h1>Non ci sono posts</h1>
+            ) : (
+
+                postsList.map((post) => (
+                    <div className='postItem' key={post.id}>
+                        <h2>{post.title}</h2>
+                        <button onClick={() => removePost(post.id)}>Cancella Post</button>
+                    </div >
+
+                ))
+            )}
+
+
         </>
-    );
+
+    )
+
 }
