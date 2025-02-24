ee977ba2ed7e98a7554ea6af328e89a328b601a0
Giovanni <dibellogio@gmail.com>
2025-02-24 15:52:16 +0100

    add PostsList component

---
+++ src/pages/PostsList.jsx
@@ -0,0 +1,12 @@
+// importiamo componenti di template
+import Header from "./../components/Header";
+
+export default function PostsList() {
+
+    return (
+        <>
+            <Header />
+            <h2>PostsList</h2>
+        </>
+    );
+}
